ff605babf19417b8b63002f3b7eee73d33f25444
Kazuo256 <kazuo256@gmail.com>
2017-04-23 16:43:02 -0300

    Connect title scene to editor

--- game/menu/title.gd
+++ game/menu/title.gd
@@ -1,5 +1,7 @@
 extends TextureFrame
 
+const EDITOR = preload("res://battle/editor/main.tscn")
+
 onready var fade = get_node("Fader")
 onready var tween = get_node("Tween")
 
@@ -8,3 +10,10 @@ func _ready():
 	var pos = get_pos()
 	tween.interpolate_method(self, "set_pos", pos + Vector2(800,0), pos, 1.3, Tween.TRANS_QUART, Tween.EASE_OUT)
 	tween.start()
+	yield(tween, "tween_complete")
+	set_process_input(true)
+
+func _input(event):
+	if event.is_action_pressed("ui_accept"):
+		emit_signal("exited")
+		replace_by(EDITOR.instance())
