8da8ba5a7b9c672966b7761f5ea7f2b7dc0bb793
Kazuo256 <kazuo256@gmail.com>
2017-04-23 16:11:56 -0300

    More adjustments to title scene

--- game/menu/main.gd
+++ game/menu/main.gd
@@ -7,5 +7,5 @@ onready var title = get_node("Title")
 func _ready():
 	fade.fade_in()
 	var pos = title.get_pos()
-	tween.interpolate_method(title, "set_pos", pos + Vector2(800,0), pos, 1.5, Tween.TRANS_QUAD, Tween.EASE_OUT)
+	tween.interpolate_method(title, "set_pos", pos + Vector2(800,0), pos, 1.3, Tween.TRANS_QUART, Tween.EASE_OUT)
 	tween.start()
