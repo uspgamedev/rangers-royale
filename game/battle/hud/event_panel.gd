aa7e557e5ee29d5a6be6528bf84c6b0917d346ce
Kazuo256 <kazuo256@gmail.com>
2017-04-23 11:28:57 -0300

    Put a limit to event panel

--- game/battle/hud/event_panel.gd
+++ game/battle/hud/event_panel.gd
@@ -10,7 +10,7 @@ const EVENTS = [
 onready var events = get_node("Events")
 
 func draw_event(main):
-	if get_child_count() < 10:
+	if events.get_child_count() < 9:
 		var new_event = EVENTS[randi() % EVENTS.size()].instance()
 		new_event.map = main.get_node("Map")
 		events.add_child(new_event)
