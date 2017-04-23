8b26d3a166db8d2f3f7f27aef5e31425c7de24d6
Victor Domiciano <victor.dm3@gmail.com>
2017-04-23 12:02:08 -0300

    Remove print

--- game/objects/player/range_area.gd
+++ game/objects/player/range_area.gd
@@ -7,7 +7,6 @@ func _ready():
 
 func set_radius(r):
 	get_child(0).get_shape().set_radius(r)
-	print("area radius: " + var2str(get_child(0).get_shape().get_radius()))
 
 func _exit_tree():
 	emit_signal("removed_from_tree")
