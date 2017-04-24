b3f0d4f641d28205c5e4f98ea924f7638674cb62
Kazuo256 <kazuo256@gmail.com>
2017-04-24 15:12:25 -0300

    Remove unused code

--- game/battle/zones/death_zone_timer.gd
+++ game/battle/zones/death_zone_timer.gd
@@ -15,9 +15,6 @@ func _activate():
 	for item in map.items_in_zone(zone):
 		item.queue_free()
 	var zones = map.get_node("Zones")
-	for tile in zone.get_used_cells():
-		var pos = zones.get_tile_at(zone.map_to_global_world(tile), false)
-		zones.set_cellv(pos, 0)
 	zone.clear()
 	zone.queue_free()
 	queue_free()
