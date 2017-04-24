8de7f027c84e89797c688c2a45f6604936fd2efa
rilifon <pudimdefrutas@gmail.com>
2017-04-24 19:32:50 -0300

    New banner messages

--- game/battle/events/medkit_event.gd
+++ game/battle/events/medkit_event.gd
@@ -2,8 +2,17 @@
 extends "res://battle/events/event_base.gd"
 
 const MEDKIT = preload("res://objects/item/med_kit.tscn")
+onready var global_state = get_node("/root/GlobalState")
+
+onready var msgs = [
+	"A medkit is landing on the battlefield! Will the tide of battle turn?",
+	"Health is literally falling from the sky!",
+	"Keep your eyes open Rangers, a medkit has appeared!",
+]
 
 func _use(pos, zone):
 	queue_free()
 	var medkit = MEDKIT.instance()
+	if randf() < .5:
+		global_state.shortcuts["newsticker"].queue_msg(msgs[randi()%msgs.size()])
 	self.map.drop_item(medkit, pos)
